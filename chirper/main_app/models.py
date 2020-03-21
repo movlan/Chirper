e28431bc33f592075cce087e13fd1c61b4c9f973
DagnyJay <59747157+DagnyJay@users.noreply.github.com>
2020-03-20 18:37:27 -0600

    Oauth login and signup

--- chirper/main_app/models.py
+++ chirper/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+
+class Avatar(models.Model):
+    url = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"photo for UserId {self.user_id}"
+
--- chirper/main_app/views.py
+++ chirper/main_app/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserCreateForm
 
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreateForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreateForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
