8aa1a512674155c75b833f587be00ed5e5e86914
DagnyJay <59747157+DagnyJay@users.noreply.github.com>
2020-03-22 12:42:50 -0600

    add update and delete functionality

--- chirper/main_app/urls.py
+++ chirper/main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('chirps/create/', views.ChirpCreate.as_view(), name='chirps_create'),
+    path('chirps/<int:pk>/update/', views.ChirpUpdate.as_view(), name='chirps_update'),
+    path('chirps/<int:pk>/delete/', views.ChirpDelete.as_view(), name='chirps_delete'),
 ]
