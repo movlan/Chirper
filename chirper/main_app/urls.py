80666c4ed666929b800615dd0a2ff3fb7fb1000a
Bahram Movlanov <bmovlanov@gmail.com>
2020-03-22 11:34:26 -0600

    Bahram Chirp working

--- chirper/main_app/urls.py
+++ chirper/main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('chirps/create/', views.ChirpCreate.as_view(), name='chirps_create'),
 ]
