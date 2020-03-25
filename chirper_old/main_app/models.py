0ff4e50ff9245d1fa45c0a9675a1b2fd17a186fa
Bahram Movlanov <bmovlanov@gmail.com>
2020-03-25 14:05:54 -0600

    heroku pre fix

---
+++ chirper_old/main_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
+class User(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+
+class Avatar(models.Model):
+    url = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"photo for UserId {self.user_id}"
+
+class Chirp(models.Model):
+    content = models.TextField(max_length=140, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+class Follower(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    
+    class Meta:
+        unique_together = ('follower', 'following')
+
+    def __str__(self):
+        return u'%s follows %s' % (self.follower, self.following)
---
+++ chirper_old/main_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('chirps/create/', views.ChirpCreate.as_view(), name='chirps_create'),
+    path('chirps/<int:pk>/update/', views.ChirpUpdate.as_view(), name='chirps_update'),
+    path('chirps/<int:pk>/delete/', views.ChirpDelete.as_view(), name='chirps_delete'),
+    path('user/<int:user_id>/add_avatar/', views.add_avatar, name='add_avatar'),
+    path('user/<int:user_id>/', views.profile, name='profile'),
+    path('follow/<int:user_id>/', views.follow, name='follow'),
+]
