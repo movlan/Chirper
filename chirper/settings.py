756e4c176a21771721bb20575429945d0858a39c
Bahram Movlanov <bmovlanov@gmail.com>
2020-03-25 15:09:24 -0600

    moved all from chirper folder to root

--- chirper/settings.py
+++ chirper/settings.py
@@ -125,6 +125,3 @@ AUTH_USER_MODEL = 'main_app.User'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
-
-import django_heroku
-django_heroku.settings(locals())
--- chirper_old/main_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, Chirp, Avatar, Follower
-
-admin.site.register(User)
-admin.site.register(Chirp)
-admin.site.register(Avatar)
-admin.site.register(Follower)
