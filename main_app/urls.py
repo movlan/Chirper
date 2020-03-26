9b7af504c47e06a56d615158a972621525590b34
Bahram Movlanov <bmovlanov@gmail.com>
2020-03-26 09:50:30 -0600

    user page css

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('chirps/create/', views.ChirpCreate.as_view(), name='chirps_create'),
     path('chirps/<int:pk>/update/', views.ChirpUpdate.as_view(), name='chirps_update'),
     path('chirps/<int:pk>/delete/', views.ChirpDelete.as_view(), name='chirps_delete'),
+    path('follow/<int:user_id>/', views.follow, name='follow'),
     path('user/<int:user_id>/add_avatar/', views.add_avatar, name='add_avatar'),
     path('user/<int:user_id>/', views.profile, name='profile'),
-    path('follow/<int:user_id>/', views.follow, name='follow'),
     path('user/<int:pk>/update/', views.UserUpdate.as_view(), name='user_update'),
     path('user/<int:pk>/delete/', views.UserDelete.as_view(), name='user_delete'),
-    url(r'^password/user_form.html', views.change_password, name='change_password'),
+    url(r'^password/$', views.change_password, name='change_password'),
 ]
